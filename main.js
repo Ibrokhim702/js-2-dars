0662a2a260c9725d65cfbc677aa0df6516f852ac
Ibrokhim702 <rakhimovaziz077@gmail.com>
2024-12-04 21:16:16 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// String typedagi malumot kiriting
+// Number typedagi malumot kiriting 
+// Boolean typedagi malumot kiriting
+
+var ism = prompt ("String typedagi malumot kiriting")
+console.log(`Siz kiritgan malumot ${typeof ism}`);
+
+var ism = +prompt ("Number typedagi malumot kiriting ")
+console.log(typeof Boolean(ism));
+ 
+var ism = prompt ("Boolean typedagi malumot kiriting")
+console.log(typeof (ism));
